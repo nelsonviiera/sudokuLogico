da8dfb90f805b929f3d9b84db3626f23805f661c
Tiago Umemura <umemuratiago@gmail.com>
2016-06-25 22:13:15 -0300

    getNum funcionando

--- sudoku.pl
+++ sudoku.pl
@@ -1,11 +1,18 @@
 %abre a base de conhecimento
 abrirBase :- consult(base_Sudoku).
 
+%printa tabuleiro.
 printf([X|Y], I, F) :- write(X), nl, A is I+1, A < F, printf(Y, A, F);
 					A is I+1, A >= F, write('matriz printada').
-%printa tabuleiro.
 
-getNum([Xn|Yn], La, Lt) :- write(Xn), La == Lt.   
+%linha e coluna alvo
+getNum(L, Li, Co, N) :- getLinha(L, Li, 0, [Linha]), getColuna(Linha, Co, 0, Nx), N is Nx.
+
+getLinha([X1|X2], Li, La, [Linha]) :- La == Li, Linha = X1; 
+                                      La < Li, Laux is La+1, getLinha(X2, Li, Laux, [Linha]).
+
+getColuna([X3|X4], Co, Ca, Nx) :- Ca == Co, Nx = X3;
+                                  Ca < Co, Caux is Ca+1, getColuna(X4, Co, Caux, Nx).
 
 iniciar :- abrirBase,nl,
      write('            Jogo Sudoku em Prolog'),nl,
@@ -13,7 +20,11 @@ iniciar :- abrirBase,nl,
      write('----------------------------------------------'),nl,
      write('Tabuleiro inicial:'), nl,
      atribui(L),
-     printf(L,0,9),
+     printf(L, 0, 9),%printar matriz
      nl,
-     getNum(L, 0, 0),
+     getNum(L, 8, 7, N),
+     nl,
+     write(N),
+     nl,
+     write('Fim'),
      fail.
